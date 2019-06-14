8acffc8ed0bc23516c56319e4a96e99c65b22a9a
chinth.sekara <chinth.sekara@gmail.com>
2019-06-15 01:10:25 +0530

    ggg

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,37 +7,18 @@ import { AlertService, AuthenticationService } from '@app/_services';
 
 @Component({templateUrl: 'login.component.html'})
 export class LoginComponent implements OnInit {
-    loginForm: FormGroup;
-    loading = false;
-    submitted = false;
-    returnUrl: string;
-
+   
     constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private authenticationService: AuthenticationService,
-        private alertService: AlertService
+      
     ) {
-        // redirect to home if already logged in
-        if (this.authenticationService.currentUserValue) { 
-            this.router.navigate(['/']);
-        }
+        
     }
 
     ngOnInit() {
-        this.loginForm = this.formBuilder.group({
-            username: ['', Validators.required],
-            password: ['', Validators.required]
-        });
-
-        // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+      
     }
 
-    // convenience getter for easy access to form fields
-    get f() { return this.loginForm.controls; }
-
+    
     onSubmit() {
         // debugger;
         // this.submitted = true;
@@ -58,7 +39,7 @@ export class LoginComponent implements OnInit {
         //             this.alertService.error(error);
         //             this.loading = false;
         //         });
-        this.router.navigate(['/home']);
+      
          
     }
 }
